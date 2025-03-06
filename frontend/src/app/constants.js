69992e60a9075f7fb8a60dca9f510ec7590b3a35
ArunKushhhh <arunkushwaha_230604@aitpune.edu.in>
2025-03-06 15:56:38 +0530

    genre/frontend
    Signed-off-by: ArunKushhhh <arunkushwaha_230604@aitpune.edu.in>

--- frontend/src/app/constants.js
+++ frontend/src/app/constants.js
@@ -2,3 +2,4 @@
 export const BASE_URL = ""; //why empty?? because already inside our vite.config, we are already setting our api to localhost 3000
 
 export const USERS_URL = "/api/v1/users";
+export const GENRE_URL = "/api/v1/genre";
