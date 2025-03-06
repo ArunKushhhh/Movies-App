80b3f4c8060694106191bab9333f880beb6b4f22
ArunKushhhh <arunkushwaha_230604@aitpune.edu.in>
2025-03-07 04:42:30 +0530

    movies/backend-2
    Signed-off-by: ArunKushhhh <arunkushwaha_230604@aitpune.edu.in>

--- backend/routes/moviesRoutes.js
+++ backend/routes/moviesRoutes.js
@@ -2,7 +2,18 @@ import express from "express";
 const router = express.Router();
 
 //Controllers
-import { createMovie, getAllMovies, getSpecificMovie, updateMovie, movieReview, deleteMovie } from "../controllers/movieController.js";
+import {
+  createMovie,
+  getAllMovies,
+  getSpecificMovie,
+  updateMovie,
+  movieReview,
+  deleteMovie,
+  deleteComment,
+  getNewMovies,
+  getTopMovies,
+  getRandomMovies,
+} from "../controllers/movieController.js";
 
 //Middlewares
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
@@ -10,13 +21,19 @@ import checkId from "../middlewares/checkId.js";
 
 //Public Routes
 router.get("/all-movies", getAllMovies);
-router.get("/specific-movie/:id", getSpecificMovie)
+router.get("/specific-movie/:id", getSpecificMovie);
+
+//RTK Query
+router.get("/new-movies", getNewMovies);
+router.get("/top-movies", getTopMovies);
+router.get("/random-movies", getRandomMovies);
 
 //Restricted Routes
-router.post('/:id/reviews', authenticate, checkId, movieReview);
+router.post("/:id/reviews", authenticate, checkId, movieReview);
 
 //Admin Routes
-router.post('/create-movie', authenticate, authorizeAdmin, createMovie);
-router.put('/update-movie/:id', authenticate, authorizeAdmin, updateMovie);
-router.delete('/delete-movie/:id', authenticate, authorizeAdmin, deleteMovie);
+router.post("/create-movie", authenticate, authorizeAdmin, createMovie);
+router.put("/update-movie/:id", authenticate, authorizeAdmin, updateMovie);
+router.delete("/delete-movie/:id", authenticate, authorizeAdmin, deleteMovie);
+router.delete("/delete-comment", authenticate, authorizeAdmin, deleteComment);
 export default router;
